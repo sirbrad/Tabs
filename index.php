2d72b28746cf9c5cabd730fb306ec62f5d50cf69
Bradly Few <few.bradley@gmail.com>
2011-08-25 09:56:43 +0100

    Tabs
    - All files pushed.
    - HTML, CSS, JavaScript

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<!-- Explanation: http://www.phpied.com/conditional-comments-block-downloads/ -->
+<!--[if IE]><![endif]-->
+<html dir="ltr" lang="en">
+<head>
+	<title>TABS</title>
+   <script src="Assets/Scripts/modernizr-1.6.min.js"></script>
+   <meta charset="utf-8">
+   <meta name="author" content="Bradley Few" />
+   <link rel="stylesheet" href="Assets/Styles/tabs.css" />
+</head>
+<?php flush(); ?>
+<body>
+   <div class="container">
+   	<ul id="tabs-nav">
+      	<li><a href="#tab1">Tab 1</a></li>
+      	<li><a href="#tab2">Tab 2</a></li>
+      </ul>
+      <div id="tabs" class="loaded">
+      	<div id="tab1" class="active">
+         	<p>Wow im inside of a freaking tab 1 right now!</p>
+         	<p>Wow im inside of a freaking tab 1 right now!</p>
+         	<p>Wow im inside of a freaking tab 1 right now!</p>
+         	<p>Wow im inside of a freaking tab 1 right now!</p>
+         	<p>Wow im inside of a freaking tab 1 right now!</p>
+         </div>
+      	<div id="tab2">
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         	<p>Wow im inside of a freaking tab 2 right now!</p>
+         </div>
+      </div>
+      <script>
+      	document.getElementById('tabs').style.display = 'none';
+      </script>	
+   </div>
+   <!-- LABjs allows loading other scripts in parallel (without blocking) by inserting script tags dynamically -->
+   <script src="Assets/Scripts/LAB-min.js"></script>
+   <script>
+		$LAB
+		.setOptions({ AlwaysPreserveOrder:true, BasePath:'Assets/Scripts/' })
+			.script('jquery.js')
+			.script('library.js')
+   </script>
+</body>
+</html>
